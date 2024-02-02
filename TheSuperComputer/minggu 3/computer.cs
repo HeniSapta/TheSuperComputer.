42987dbba416e7d0d66cc8678aa7a87234bff809
HeniSapta <152345387+HeniSapta@users.noreply.github.com>
2024-02-02 10:31:03 +0700

    Add files via upload

---
+++ TheSuperComputer/minggu 3/computer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSuperComputer
+{
+    class computer
+    {
+        private Monitor monitor;
+        private Printer printer;
+        private Processor processor;
+        private Ram ram;
+        private Vga vga;
+
+        public computer(StringBuilder builder){
+            this.monitor = builder.monitor;
+            this.series = series;
+            this.ppm = ppm;
+        }
+}
---
+++ TheSuperComputer/minggu 3/processor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSuperComputer
+{
+    class processor
+    {
+        //processor : series, core, cache
+        private String series;
+        private int core;
+        private int cache;
+
+        public processor(){
+        
+        }
+
+        public void setSeries(String series){
+            this.series = series;
+        }
+
+        public String getString(){
+            return this.series;
+        }
+
+        public void setCore(int core){
+            this.core = core;
+        }
+
+        public int getCore(){
+            return this.core;
+        }
+
+        public void setCache(int cache){
+            this.cache = cache;
+        }
+
+        public int getCache(){
+            return this.cache;
+        }
+
+        public override string ToString()
+        {
+            return $"Processor series: {this.series} ; Processor core: {this.core}; Processor cache:{this.cache}";
+        }
+    }
+}
